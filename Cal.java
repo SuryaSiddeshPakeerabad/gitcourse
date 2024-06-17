d9b79ba12d3904cf46ba9779cc30ddfc64f95450
Surya <Suryapk2002@gmail.com>
2024-06-17 15:05:11 +0530

    sub added to java file

---
+++ Cal.java
@@ -0,0 +1,17 @@
+class Cal{
+    public static void main(String[] args) {
+        int a=10,b=10;
+        sum(a, b);
+        sub(a, b);
+
+        
+    }
+    static void sum(int a,int b)
+    {
+        System.out.println(a+b);
+    }
+    static void sub(int a,int b)
+    {
+        System.out.println(a-b);
+    }
+}
